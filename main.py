e497edec9fe89f0a92e23197c08b912926af9c11
svandersar <stefanvandersar10@gmail.com>
2016-04-20 13:34:42 +0200

    Implicit method and double aperture potential

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def calculateExplicit(x, t, dx, dt, psi, V):
     # Periodic boundary condition
     H[0,-1] = -1/(2 * dx**2)
     H[-1,0] = -1/(2 * dx**2)
+    
     # Add potential
     H += np.diag(V,0)
 
@@ -19,6 +20,22 @@ def calculateExplicit(x, t, dx, dt, psi, V):
         # Compute next time step
         psi[k+1,:] = psi[k,:] + dt/(1j) * np.dot(H,psi[k,:])
 
+    return psi
+    
+def calculateImplicit(x,t,dx,dt,psi,V):
+    #Construct hamiltonian
+    diagonals=[-2*np.ones(len(x)), np.ones(len(x)-1), np.ones(len(x)-1)] 
+    H = -1/(2 * dx**2) * scipy.sparse.diags(diagonals, [0,1,-1], format="csc")
+    H += scipy.sparse.diags(V,0,format="csc")    
+    H2 = 1j/dt*scipy.sparse.identity(len(x), format="csc") - H
+    
+    #Invert hamiltonian    
+    H2Inv=scipy.sparse.linalg.inv(H2)    
+    
+    #Compute next time step
+    for k in range(0, len(t)-1):
+        psi[k+1,:] = H2Inv.dot(1j/dt*psi[k,:])
+        
     return psi
 
 def calculateSuzuki(x, t, dx, dt, psi, V):
@@ -47,8 +64,8 @@ xmax = 3*math.pi;
 dx = .01;
 
 tmin = 0
-tmax = 500.0
-dt = 1
+tmax = 25
+dt = 0.005
 
 # Determine x and t range
 x = np.arange(xmin, xmax, dx)
@@ -59,6 +76,9 @@ X, T =np.meshgrid(x,t)
 # Define psi at t=0
 psi0 = np.zeros(len(x));
 psi0[int(len(x)*1/3):int(len(x)*2/3)] = np.sin( x[int(len(x)*1/3):int(len(x)*2/3)] - math.pi   )
+#Normalize psi
+Norm=scipy.integrate.trapz(psi0**2,dx=dx)
+psi0*=1/(Norm**(1/2))
 
 # Define potential
 V = np.zeros(len(x))
@@ -69,6 +89,14 @@ V[int(len(V)*2/3):len(V)] = 9223372036854775807;
 #V[0:int(len(V)/4)] = 9223372036854775807; # Max int?
 #V[int(len(V)*3/4):len(V)] = 9223372036854775807;
 
+# Double aperture potential
+#a=0.3; #distance between apertures
+#d=0.1; #diameter of apertures
+#V[0:int((0.5-a/2-d/2)*len(V))] = 9223372036854775807
+#V[int((0.5-a/2+d/2)*len(V)):int((0.5+a/2-d/2)*len(V))]=9223372036854775807
+#V[int((0.5+a/2+d/2)*len(V)):-1]=9223372036854775807
+
+
 # Inititate psi
 psi = np.array(np.zeros([len(t),len(x)]), dtype=np.complex128)
 psi[0,:] = psi0
@@ -76,22 +104,37 @@ psi[0,:] = psi0
 # Explicit method
 psiExplicit = calculateExplicit(x, t, dx, dt, np.copy(psi), V);
 
+#Implicit mehod
+psiImplicit = calculateImplicit(x, t, dx, dt, np.copy(psi), V)
+
 # Suzuki/Souflaki/Gyros Method
 psiGyros = calculateSuzuki(x, t, dx, dt, np.copy(psi), V);
 
 # Plot results
-fig = plt.figure(1)
-ax = fig.gca(projection='3d')
-wire=ax.plot_wireframe(X,T,np.real(psiExplicit),rstride=1, cstride=1)
-plt.xlabel("Position")
-plt.ylabel("Time")
+#fig = plt.figure(1)
+#ax = fig.gca(projection='3d')
+#wire=ax.plot_wireframe(X,T,np.real(psiExplicit),rstride=1, cstride=1)
+#plt.xlabel("Position")
+#plt.ylabel("Time")
 
+
+#fig = plt.figure(2)
+#ax = fig.gca(projection='3d')
+#wire=ax.plot_wireframe(X,T,np.real(psiGyros),rstride=1, cstride=1)
+#plt.xlabel("Position")
+#plt.ylabel("Time")
 
 fig = plt.figure(2)
 ax = fig.gca(projection='3d')
-wire=ax.plot_wireframe(X,T,np.real(psiGyros),rstride=1, cstride=1)
+surf = ax.plot_surface(X, T, np.real(psiImplicit), rstride=10, cstride=10,cmap=cm.coolwarm, linewidth=0, antialiased=False)
+ax.view_init(90, 90); # Top view
 plt.xlabel("Position")
 plt.ylabel("Time")
+fig.colorbar(surf, shrink=0.5, aspect=5)
+# Hide z-axis
+ax.w_zaxis.line.set_lw(0.)
+ax.set_zticks([])
+
 
 fig = plt.figure(3)
 ax = fig.gca(projection='3d')
@@ -104,6 +147,7 @@ fig.colorbar(surf, shrink=0.5, aspect=5)
 ax.w_zaxis.line.set_lw(0.)
 ax.set_zticks([])
 
+
 plt.figure(4)
 plt.plot(x,V);
 plt.xlabel('Position')
@@ -112,6 +156,4 @@ plt.ylabel("Potential")
 plt.figure(5)
 plt.plot(x,psi0);
 plt.xlabel('Position')
-plt.ylabel("Initial Psy")
-
-
+plt.ylabel("Initial Psi")
