9922a444821236ff07da0aca93a02079a9dbcefc
mgarming <mgarming@outlook.com>
2016-04-21 14:02:20 +0200

    Print progress

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import scipy
 from matplotlib import cm
 
 def calculateExplicit(x, t, dx, dt, psi, V):
+    print("Calculating explicit method...",flush=True)
     # Construct hamiltonian
     H = -1/(2 * dx**2) * (-2*np.diag(np.ones(len(x)), 0) + np.diag(np.ones(len(x)-1), 1) + np.diag(np.ones(len(x)-1), -1))
     # Periodic boundary condition
@@ -19,10 +20,12 @@ def calculateExplicit(x, t, dx, dt, psi, V):
     for k in range(0, len(t)-1):
         # Compute next time step
         psi[k+1,:] = psi[k,:] + dt/(1j) * np.dot(H,psi[k,:])
-
-    return psi
+    
+    print("Done",flush=True)
+    return psi   
     
 def calculateImplicit(x,t,dx,dt,psi,V):
+    print("Calculating implicit method...",flush=True)
     #Construct hamiltonian
     diagonals=[-2*np.ones(len(x)), np.ones(len(x)-1), np.ones(len(x)-1)] 
     H = -1/(2 * dx**2) * scipy.sparse.diags(diagonals, [0,1,-1], format="csc")
@@ -35,10 +38,13 @@ def calculateImplicit(x,t,dx,dt,psi,V):
     #Compute next time step
     for k in range(0, len(t)-1):
         psi[k+1,:] = H2Inv.dot(1j/dt*psi[k,:])
-        
+    
+    print("Done",flush=True)  
     return psi
-
+    
 def calculateSuzuki(x, t, dx, dt, psi, V):
+    print("Calculating FT method...",flush=True)    
+    
     exp1 = scipy.linalg.expm(-1j*dt/2 * np.diag(V,0));
 
     L = (max(x)-min(x))
@@ -55,9 +61,11 @@ def calculateSuzuki(x, t, dx, dt, psi, V):
         t4 = np.fft.ifft(t3);
         psi[k,:] = np.dot(exp1, t4);
         #print(sum(psi[k,:]))
-
+    
+    print("Done",flush=True)
     return psi
-
+        
+    
 # Define input parameters
 xmin = 0;
 xmax = 3*math.pi;
@@ -102,7 +110,7 @@ psi = np.array(np.zeros([len(t),len(x)]), dtype=np.complex128)
 psi[0,:] = psi0
 
 # Explicit method
-psiExplicit = calculateExplicit(x, t, dx, dt, np.copy(psi), V);
+#psiExplicit = calculateExplicit(x, t, dx, dt, np.copy(psi), V);
 
 #Implicit mehod
 psiImplicit = calculateImplicit(x, t, dx, dt, np.copy(psi), V)
@@ -130,6 +138,7 @@ surf = ax.plot_surface(X, T, np.real(psiImplicit), rstride=10, cstride=10,cmap=c
 ax.view_init(90, 90); # Top view
 plt.xlabel("Position")
 plt.ylabel("Time")
+plt.title("Implicit method")
 fig.colorbar(surf, shrink=0.5, aspect=5)
 # Hide z-axis
 ax.w_zaxis.line.set_lw(0.)
@@ -142,6 +151,7 @@ surf = ax.plot_surface(X, T, np.real(psiGyros), rstride=10, cstride=10,cmap=cm.c
 ax.view_init(90, 90); # Top view
 plt.xlabel("Position")
 plt.ylabel("Time")
+plt.title("FT method")
 fig.colorbar(surf, shrink=0.5, aspect=5)
 # Hide z-axis
 ax.w_zaxis.line.set_lw(0.)
